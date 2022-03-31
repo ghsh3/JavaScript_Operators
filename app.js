fd8ac518efe301b9c696180073aec1e461cf21c7
Sharry Hayes <sharryah@yahoo.com>
2022-03-31 07:49:51 -0600

    JS Operators Exercise

--- app.js
+++ app.js
@@ -6,14 +6,87 @@ console.log(
 // Exercise 1
 console.log("EXERCISE 1:\n==========\n");
 
+let a = 20;
+let b = 4;
+let add = a + b; 
+let minus = a - b;
+let multiply = a * b;
+let dividing = a / b;
+
 // YOUR CODE HERE
+console.log(a);
+console.log(b);
+console.log(add);
+console.log(minus);
+console.log(multiply);
+console.log(dividing);
+
+
 
 // Exercise 2
 console.log("EXERCISE 2:\n==========\n");
 
+let c = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Jackie";
+let lastName = "Chan";
+
 // YOUR CODE HERE
+console.log(c)
+console.log(c + str);
+console.log(c + str2);
+console.log(c + isPresent);
+console.log(c + firstName);
+console.log(c + lastName);
+
+/*
+What is the value of: a + str?  "1111"
+What is the value of: a + str2? "11eleven"
+What is the value of: a + isPresent? 12-?
+What is the value of: a + firstName? 11Jackie
+What is the value of: a + lastName?  11Chan
+*/
+
+
 
 // Exercise 3
 console.log("EXERCISE 3:\n==========\n");
+
+let d = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
+let firstName2 = "Robin";
+let lastName2 = "Williams";
+
+
+console.log(d == str3);
+console.log(d === str4);
+console.log(!isPresent2);
+console.log("eleven" == str3);
+console.log(d >= str4)
+console.log("eleven" == str3 && d >= str4);
+console.log(!isPresent2 || isPresent2);
+console.log(0 == false);
+console.log(0 === false);
+console.log(0 != false);
+console.log(0 !== false);
+
+/*
+What is the value of: d == str3?
+What is the value of: d === str4?
+What is the value of: !isPresent2?
+What is the value of: "eleven" == str3)? false
+What is the value of: d >= str4? false
+What is the value of: (“eleven” == str3 && d >= str4)?
+What is the value of: (!isPresent2 || isPresent2)?
+What is the value of: 0 == false?
+What is the value of: 0 === false?
+What is the value of: 0 != false?
+What is the value of 0 !== false?
+*/
+
 
 // YOUR CODE HERE
